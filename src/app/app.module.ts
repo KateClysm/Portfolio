2b164e1dcb1b9be1fa3d1a3ce129759ae55b0785
Katerina <kateclysm@gmail.com>
2022-10-27 13:05:21 -0300

    Secciones => Componentes en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { BanerComponent } from './componentes/baner/baner.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BanerComponent,
+    ExperienciaComponent,
+    ContactoComponent,
+    FooterComponent,
+    SobremiComponent,
   ],
   imports: [
     BrowserModule,
