a3a5def73390cc676576bc7127f5fb422847145e
Katerina <kateclysm@gmail.com>
2022-12-11 22:42:01 -0300

    B de Login

--- FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/src/app/app-routing.module.ts
@@ -4,21 +4,21 @@ import { RouterModule, Routes } from '@angular/router';
 //Importamos nuestros componentes
 import { HomeComponent } from './components/home/home.component';
 import { InfoComponent } from './components/info/info.component';
-import { ExperienceComponent } from './components/experience/experience.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { LoginComponent } from './components/login/login.component';
 import { CreditsComponent } from './components/credits/credits.component';
 import { Page404Component } from './components/page404/page404.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 
-//Definimos nuestras rutas
+//Mis rutas
 const routes: Routes = [
   {path: '', redirectTo: '/Inicio', pathMatch: 'full'},
   {path: 'Inicio', component: HomeComponent },
   {path: 'Sobre-Mi', component: InfoComponent },
-  {path: 'Experiencia-Y-Proyectos', component: ExperienceComponent},
+  {path: 'Experiencia-Y-Proyectos', component: ExperienciaComponent},
   {path: 'Contacto', component: ContactComponent},
-  {path: 'Acceso', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'Creditos', component: CreditsComponent},
   {path: '**', component: Page404Component}
 ];
--- FRONTEND/src/app/app.module.ts
+++ FRONTEND/src/app/app.module.ts
@@ -1,24 +1,20 @@
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-//
-import { BrowserModule } from '@angular/platform-browser';
-//
-import { HttpClientModule } from '@angular/common/http';
-//
-import { FormsModule } from '@angular/forms';
-//Rutas
-import { AppRoutingModule } from './app-routing.module';
-//Components
-import { NavComponent } from './components/nav/nav.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { CreditsComponent } from './components/credits/credits.component';
-import { HomeComponent } from './components/home/home.component';
-import { InfoComponent } from './components/info/info.component';
-import { ExperienceComponent } from './components/experience/experience.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { LoginComponent } from './components/login/login.component';
-import { Page404Component } from './components/page404/page404.component';
-//
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { CreditsComponent } from "./components/credits/credits.component";
+import { ExperienciaComponent } from "./components/experiencia/experiencia.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from "./components/home/home.component";
+import { InfoComponent } from "./components/info/info.component";
+import { LoginComponent } from "./components/login/login.component";
+import { NavComponent } from "./components/nav/nav.component";
+import { Page404Component } from "./components/page404/page404.component";
+import { interceptorProvider } from "./services/interceptor-service";
+
 
 
 //Angular define los módulos como clases a través del decorador @NgModule.
@@ -30,22 +26,22 @@ import { Page404Component } from './components/page404/page404.component';
     CreditsComponent,
     HomeComponent,
     InfoComponent,
-    ExperienceComponent,
+    ExperienciaComponent,
     ContactComponent,
     LoginComponent,
-    Page404Component
+    Page404Component,
   ],
   //Modulos necesarios para que éste módulo funcione correctamente
     imports: [
-    BrowserModule, //
-    HttpClientModule, //
-    AppRoutingModule, //módulo de rutas
-    BrowserModule,
-    FormsModule, 
-    
+    BrowserModule, 
+    HttpClientModule, 
+    AppRoutingModule,
+    FormsModule,     
   ],
   //Proveedores de servicios necesarios
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
