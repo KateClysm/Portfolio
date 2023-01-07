115d9a7cd928eeec637651f4ee3cbc1352c3ef21
Katerina <kateclysm@gmail.com>
2023-01-07 16:14:04 -0300

    Configuraciones

--- src/app/services/persona.service.ts
+++ src/app/services/persona.service.ts
@@ -10,7 +10,7 @@ import { persona } from '../model/persona.model';
 })
 
 export class PersonaService {
-  authURL = 'https://localhost:8080/persona/'
+  authURL = 'https://localhost:8080/personas/'
 
   constructor(private httpClient: HttpClient) { }
 
